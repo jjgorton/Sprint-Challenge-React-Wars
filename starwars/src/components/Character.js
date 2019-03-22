6f0d9583befd7f0c8d96f4f550eab2e686a6826a
jjgorton <jjgorton@gmail.com>
2019-03-22 12:57:53 -0400

    character attributes listed out, ready to style

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -6,6 +6,17 @@ const Character = (props) => {
 	return (
 		<div className="character-container">
 			<h3>{props.charData.name}</h3>
+			<div className="char-attr">
+				<li>Born: {props.charData.birth_year}</li>
+				<li>Gender: {props.charData.gender}</li>
+				<li>Height: {props.charData.height}</li>
+				<li>Weight: {props.charData.mass}</li>
+				<li>Eye Color:{props.charData.eye_color}</li>
+				<li>Hair Color: {props.charData.hair_color}</li>
+				<li>Skin Color: {props.charData.skin_color}</li>
+			</div>
+
+			{/* {props.charData.films.map((movie, index) => <li key={index}>{movie}</li>)}; */}
 		</div>
 	);
 };
--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Character from './Character';
 
 const CharacterList = (props) => {
+	console.log(props);
 	return (
 		<div className="character-list">
 			{props.starwarsChars.map((character, index) => <Character key={index} charData={character} />)}
